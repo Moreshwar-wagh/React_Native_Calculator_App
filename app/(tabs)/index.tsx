e026ec5ca9af347aca2bec55e0a98ae460190042
Moreshwar Wagh <moreshwarswagh@gmail.com>
2024-11-16 11:21:28 +0530

    [Updated]: Code refactored

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View, Switch, SafeAreaView } from "react-native";
 import { ThemeContext } from "@/src/context/ThemeContext";
 import { useState } from "react";
 import { myColors } from "@/src/styles/Colors";
-import { Button } from "@/components/Button";
 import MyKeyboard from "@/src/components/MyKeyboard";
 
 export default function HomeScreen() {
